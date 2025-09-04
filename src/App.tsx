290a18b56b6df95585a5193df8527c480fc6f277
Fabio Delong <frdelong@gmail.com>
2025-09-04 03:31:10 -0300

    feat: update App component to conditionally render challenge and dynamically display letters

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,10 @@ export default function App() {
     startGame()
   })
 
+  if (!challenge) {
+    return
+  }
+
   return (
     <div className={styles.container}>
       <main>
@@ -43,11 +47,9 @@ export default function App() {
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
 
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="E" />
-          <Letter value="A" />
-          <Letter value="C" />
-          <Letter value="T" />
+          {challenge.word.split("").map(() => (
+            <Letter value="" />
+          ))}
         </div>
 
         <h4>Palpite</h4>
