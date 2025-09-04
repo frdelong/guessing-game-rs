f63cfbe7f1cb8b756c4e225d08635e0c064245c5
Fabio Delong <frdelong@gmail.com>
2025-09-04 01:07:09 -0300

    feat: add Letter component with styles and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function App() {
   function handleRestartGame() {
@@ -12,7 +13,14 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   )
