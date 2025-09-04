f11afafcf492a96aebeedc815aed82232626012f
Fabio Delong <frdelong@gmail.com>
2025-09-04 01:16:02 -0300

    feat: wrap Letter components in a div for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,13 @@ export default function App() {
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
 
-        <Letter value="R" />
-        <Letter value="E" />
-        <Letter value="A" />
-        <Letter value="C" />
-        <Letter value="T" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
